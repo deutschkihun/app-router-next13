1f67388b4e396427f9a1b7223bcba0b8722b70f2
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2023-05-15 21:32:40 +0900

    Catch up next13 server action

---
+++ src/app/home/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Home() {
+  async function conosleLog() {
+    "use server";
+    console.log("hello");
+  }
+
+  return (
+    <form action={conosleLog}>
+      <button>console log</button>
+    </form>
+  );
+}
+
+// Error: Functions cannot be passed directly to Client Components unless you explicitly expose it by marking it with "use server".
+//   <form action={function} children=...>
+// check next.config.js
